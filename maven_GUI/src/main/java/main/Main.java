bfac33d787ac69ba907fb75a9bd7a484ac15d5f4
Dimitrije <dsejat@gmail.com>
2020-11-02 15:26:41 +0100

    meow

--- maven_GUI/src/main/java/main/Main.java
+++ maven_GUI/src/main/java/main/Main.java
@@ -12,6 +12,8 @@ public class Main {
 	public static void main(String[] args) {
 		GlavniProzor.getProzor().setVisible(false);
 		
+		boolean alahu = true;
+		
 		boolean novoSkladiste = true;
 		boolean isAuto = true;
 		
